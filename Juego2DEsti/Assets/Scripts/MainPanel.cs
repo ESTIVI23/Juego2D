5eabe93a17cef14ec8993ebeed541dbf12bbaa19
ESTIVI23 <ramirezvictorh23@gmail.com>
2023-11-02 21:32:04 -0300

    menu terminado
    fue correjido el error del mute

--- Juego2DEsti/Assets/Scripts/MainPanel.cs
+++ Juego2DEsti/Assets/Scripts/MainPanel.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
 
 
 public class MainPanel : MonoBehaviour
@@ -28,6 +30,17 @@ public class MainPanel : MonoBehaviour
         volumeMaster.onValueChanged.AddListener(ChangeVolumeMaster);
     }
 
+    public void PlayLevel(string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+
+    
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
     public void SetMute()
     {
 
